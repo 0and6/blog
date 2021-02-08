295196ce118de1c79e1eb7982912ac57a675dd11
Armando Martinez <0and6@outlook.com>
2021-02-08 04:12:48 -0600

    Página simple para insertar nuevas entradas

--- vistas/publicar.php
+++ vistas/publicar.php
@@ -1,27 +1,34 @@
 
 <?php
 include_once "header.php";
+include_once "database.php";
 ?>
 
 
 <?php
+$db = new Database();
 $titulo = "";
 if(isset($_POST['titulo'])) {
     $titulo = $_POST['titulo'];
     echo $_POST['titulo'];
+
+
+    $db->sentencia("INSERT INTO posts(titulo, descripcion, contenido, url, autor, categorias) values(?,?,?, ?,1, '1')", array($_POST['titulo'], $_POST['descripcion'], $_POST['contenido'], $_POST['url']));
+
 }
 ?>
 <section id="entradas">
     <p>Vamos a crear una nueva entrada</p>      
     <form action="#" method="POST">
-        <label for="titulo">Titulo</label><br><input type="text" name="titulo" value="<?php echo $titulo;?>">
+        <label for="titulo">Titulo</label><br><input type="text" name="titulo" value="<?php echo $titulo;?>" required>
         <br>
-        <label for="contenido">Contenido</label><br><textarea name="contenido"></textarea>
+        <label for="contenido">Contenido</label><br><textarea name="contenido" required></textarea>
         <br>
         <label for="descripcion">Descripcion</label>
         <br>
-        <textarea name="descripcion" id=""></textarea>
+        <textarea name="descripcion" id="" required></textarea>
         <br>
+        <label for="url">Url</label><br><input type="text" name="url" required>
         <input type="submit">
     </form>
 </section>
