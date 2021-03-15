bacf0bffce07bbbb8c1b5d32cf22c88db0ce549d
Armando Martinez <0and6@outlook.com>
2021-03-15 01:08:07 -0600

    Agregamos un archivo para gestionar las consultas a la base de datos

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
 include_once "database.php";
-$db = new Database();
+include_once "modelos/indexmodel.php";
+$model = new IndexModel();
 $configs = include('config.php');
 $url = $_SERVER['REQUEST_URI'];
 $url = strtolower($url);
@@ -19,8 +20,8 @@ $pagina = 1;
 switch($parametros[$indice]):
     case "posts":
         $nombre = count($parametros) >= 4 ? $parametros[$indice + 1] : "none";      
-        $sentencia = obtenerPosts();
-        $resultado = $db->sentencia($sentencia, array($nombre));
+        
+        $resultado = $model->obtenerPosts($nombre);
         if(count($resultado) == 0) {
             $mensaje = "La entrada no existe o la url esta mal";
             include "vistas/error_1.php";
@@ -31,12 +32,8 @@ switch($parametros[$indice]):
 
     case "categoria":
         
-        $categoria = count($parametros) >= 4 ? $parametros[$indice + 1] : "none"; 
-        $sentencia = obtenerPostsTotalesCategoria();
-        $resultado = $db->sentencia($sentencia, array($categoria));
-        
-        //$resultado = $db->paginacionEntradas($sentencia, $limit, $offset);
-        $resultado = $db->sentencia($sentencia, array($categoria));
+        $categoria = count($parametros) >= 4 ? $parametros[$indice + 1] : "none";
+        $resultado = $model->obtenerPostsTotalesCategoria($categoria);
         include_once "vistas/categorias.php";
 
         break;
@@ -45,24 +42,18 @@ switch($parametros[$indice]):
     case $parametros[$indice] == "index.htm":
     case $parametros[$indice] == "index":
     case $parametros[$indice] == "":
-        $sentencia = obtenerPostsTotales();
         
-        $resultado = $db->paginacionEntradas($sentencia, $limit, $offset);
+        $resultado = $model->obtenerPostsTotales($limit, $offset);
         include_once "vistas/principal.php";
     break;
     case "pagina":
-        $sentencia = "SELECT count(id) FROM posts";
-        $entradasTotales = $db->seleccionar($sentencia)->fetchAll();
-        $entradasTotales = intval($entradasTotales[0][0]);
+        $entradasTotales = $model->entradasTotales();
         $sentencia = obtenerPostsTotales();
         if(isset($parametros[$indice+1]) && !empty($parametros[$indice+1]) && $parametros[$indice+1] > 0) {
             $offset = ($parametros[$indice+1] - 1) * $limit;
             $pagina = $parametros[$indice+1];
         }
         $resultado = $db->paginacionEntradas($sentencia, $limit, $offset);
-        
-        //$resultado = $db->seleccionar($sentencia);
-
         include_once "vistas/principal.php";
         break;
     case "publicar":
@@ -70,20 +61,17 @@ switch($parametros[$indice]):
         break;
     case "editar":
         $entrada = $parametros[$indice + 1];
-
-        $sentencia = obtenerEditarPost();
+        $sentencia = $model->obtenerEditarPost();
         $resultado = $db->sentencia($sentencia, array($entrada));
         include "vistas/editar.php";
         break;
     case "autor":
-        $alias = count($parametros) >= 4 ? $parametros[$indice + 1] : "none";  
-        $sentencia = obtenerPostsTotalesAutor();
-        $resultado = $db->sentencia($sentencia, array($alias));
+        $alias = count($parametros) >= 4 ? $parametros[$indice + 1] : "none";
+        $resultado = $model->obtenerPostsTotalesAutor($alias);
         include_once "vistas/autores.php";
         break;
     default:
-        $sentencia = obtenerPostsTotales();
-
+        $sentencia = $model->obtenerPostsTotales();
         $resultado = $db->paginacionEntradas($sentencia, $limit, $offset);
         include_once "vistas/principal.php";
     break;
--- modelos/indexmodel.php
+++ modelos/indexmodel.php
@@ -1,26 +1,45 @@
 <?php
 
-function obtenerPostsTotales() {
-    return "SELECT posts.titulo, posts.url, posts.fecha_pub, posts.descripcion,"
+
+
+class IndexModel {
+
+    private $db;
+
+    function __construct() {
+        $this->db = new Database();
+    }
+function obtenerPostsTotales($limit, $offset) {
+    $sentencia = "SELECT posts.titulo, posts.url, posts.fecha_pub, posts.descripcion,"
     . "autores.nombre as autor, autores.alias as alias, categorias.nombre as categoria, categorias.url as categoriaurl FROM posts inner" .
     " join autores on posts.autor = autores.id inner join categorias on posts.categorias = " 
     . "categorias.id ORDER BY fecha_pub DESC LIMIT ? OFFSET ?";
+    return $this->db->paginacionEntradas($sentencia, $limit, $offset);
+    
 }
 
-function obtenerPostsTotalesAutor() {
-    return "SELECT posts.titulo, posts.url, posts.fecha_pub, posts.descripcion,"
+function obtenerPostsTotalesAutor($alias) {
+    $sentencia = "SELECT posts.titulo, posts.url, posts.fecha_pub, posts.descripcion,"
     . " autores.nombre as autor, autores.alias as alias, categorias.nombre as categoria, categorias.url as categoriaurl from posts inner join autores on posts.autor = "
     . "autores.id  inner join categorias on posts.categorias = categorias.id "
     . " where autores.alias = ? ORDER by posts.fecha_pub DESC";
 
+    return $this->db->sentencia($sentencia, array($alias));
+}
+
+function entradasTotales() {
+    $sentencia = "SELECT count(id) FROM posts";
+    $resultado = $this->db->seleccionar($sentencia)->fetchAll();
+    return intval($resultado[0][0]);
 }
 
 
-function obtenerPostsTotalesCategoria() {
-    return "SELECT posts.titulo, posts.url, posts.fecha_pub, posts.descripcion,"
+function obtenerPostsTotalesCategoria($categoria) {
+    $sentencia = "SELECT posts.titulo, posts.url, posts.fecha_pub, posts.descripcion,"
         . " autores.nombre as autor, autores.alias as alias, categorias.nombre as categoria, categorias.url as categoriaurl from posts inner join autores on posts.autor = "
         . "autores.id  inner join categorias on posts.categorias = categorias.id "
         . " where categorias.nombre = ? ORDER by posts.fecha_pub DESC";
+    return $this->db->sentencia($sentencia, array($categoria));
 }
 
 function obtenerEditarPost() {
@@ -29,9 +48,11 @@ function obtenerEditarPost() {
     .' inner join categorias on posts.categorias = categorias.id WHERE posts.id = ?';
 }
 
-function obtenerPost() {
-    return 'SELECT posts.titulo, posts.fecha_pub, posts.contenido, autores.nombre as autor,'
+function obtenerPosts($nombre) {
+    $sentencia = 'SELECT posts.titulo, posts.fecha_pub, posts.contenido, autores.nombre as autor,'
     .' autores.alias as alias, categorias.nombre as categoria, categorias.url as categoriaurl FROM posts inner join autores on posts.autor = autores.id '
     .' inner join categorias on posts.categorias = categorias.id WHERE posts.url = ?';
+    return $this->db->sentencia($sentencia, array($nombre));
+}
 }
 ?>
